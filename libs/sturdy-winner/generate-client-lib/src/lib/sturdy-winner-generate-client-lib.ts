7cc980bbee763fd06f402cf4f85b8aedefba2819
CodeYourFaceOff <codeyourfaceoff@gmail.com>
2022-02-17 14:18:03 -0600

    feat(generate-client-lib): :children_crossing: pass orval opts
    extend the incoming props object to better match the orval config options. this will allow breaking up of schemas and types better

--- libs/sturdy-winner/generate-client-lib/src/lib/sturdy-winner-generate-client-lib.ts
+++ libs/sturdy-winner/generate-client-lib/src/lib/sturdy-winner-generate-client-lib.ts
@@ -1,5 +1,6 @@
 import { OpenAPIObject } from '@nestjs/swagger';
 import { generate } from 'orval';
+import type { Options as SwaggerOptions } from '@apidevtools/swagger-parser';
 import { rm, writeFile } from 'fs/promises';
 import { tmpdir } from 'os';
 import { resolve } from 'path';
@@ -15,17 +16,45 @@ export async function generateClientLib(
     output: {
       target: props.path,
       client: props.client,
+      mode: props.mode,
+      schemas: props.schemas,
+      workspace: props.workspace,
+      mock: props.mock,
+      clean: props.clean,
+      prettier: props.prettier,
+      tsconfig: props.tsconfig,
+      override: props.outputOverride,
     },
     input: {
       target: tmpPath,
+      parserOptions: props.parser,
+      validation: props.validation,
+      converterOptions: props.converterOptions,
+      override: props.inputOverride,
     },
   });
 
   await rm(tmpPath);
 }
 
-export interface IGenerateReactQueryProps {
+// Using 'any' here due to the fact that we can't extend the options from orval
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Override = any;
+
+export type IGenerateReactQueryProps = {
   path: string;
+  schemas?: string;
+  workspace?: string;
+  mock?: boolean;
+  clean?: boolean | string[];
+  prettier?: boolean;
+  tsconfig?: string;
+  outputOverride?: Override;
+  inputOverride?: Override;
+  /**
+   * @default 'single'
+   */
+  mode?: 'single' | 'split' | 'tags' | 'tags-split';
   client:
     | 'axios'
     | 'axios-functions'
@@ -33,4 +62,12 @@ export interface IGenerateReactQueryProps {
     | 'react-query'
     | 'svelte-query'
     | 'vue-query';
-}
+
+  validation?: boolean;
+  converterOptions?: boolean;
+  parser?: SwaggerParserOptions;
+};
+
+export type SwaggerParserOptions = Omit<SwaggerOptions, 'validate'> & {
+  validate?: boolean;
+};
