d0a6e143ef4e5e54083211fd578f50e96e4ed345
CodeYourFaceOff <codeyourfaceoff@gmail.com>
2022-02-17 15:40:15 -0600

    feat(sturdy-winner-bootstrap-server): close app after res ends

--- libs/sturdy-winner/bootstrap-server/src/lib/bootstrap-server.ts
+++ libs/sturdy-winner/bootstrap-server/src/lib/bootstrap-server.ts
@@ -1,7 +1,7 @@
 import { INestApplication, NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Server } from 'http';
-import { NextApiHandler } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { extractApiRouteFromPath } from './extract-api-route-from-path';
 
 export function bootstrapServer<HandlerType = unknown>({
@@ -9,8 +9,8 @@ export function bootstrapServer<HandlerType = unknown>({
   path = extractApiRouteFromPath() || 'api/route',
   options = { bodyParser: false }, // default to not use nestjs' bodyParsing, in favor of nextjs' default
   onCreate,
-}: BootstrapServerConfig): NextApiHandler<HandlerType> {
-  const handler: NextApiHandler = async (req, res) => {
+}: BootstrapServerConfig): NextApiHandlerWithNest<HandlerType> {
+  const handler: NextApiHandlerWithNest = async (req, res) => {
     const app = await NestFactory.create(module, options);
 
     // because our routes are served under `/api`
@@ -24,7 +24,11 @@ export function bootstrapServer<HandlerType = unknown>({
     // only grab 1 handler, because we can't reuse req/res across multiple handles
     const [requestHandler] = server.listeners('request');
 
-    await requestHandler(req, res);
+    res.on('close', () => app.close());
+
+    requestHandler(req, res);
+
+    return app;
   };
 
   return handler;
@@ -35,4 +39,11 @@ export interface BootstrapServerConfig {
   path?: string;
   options?: NestApplicationOptions;
   onCreate?: (app: INestApplication) => void | Promise<void>;
+}
+
+export interface NextApiHandlerWithNest<ResponseType = any> {
+  (
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+  ): Promise<INestApplication>;
 }
