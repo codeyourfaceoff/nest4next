0e2e7e824e7ebc94884cc0800c9fe07d4890c7bf
cyfo <cyfo@cyfo.com>
2022-03-22 14:03:16 -0500

    feat(prisma): :sparkles: generate lib
    Ran:
    
    ```
    npx nx generate @nrwl/js:library \
    --name=prisma \
    --compiler=swc \
    --testEnvironment=node \
    --no-interactive
    ```

---
+++ libs/prisma/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'prisma',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': '@swc/jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/prisma',
+};
